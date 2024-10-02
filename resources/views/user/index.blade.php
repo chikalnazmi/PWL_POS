dc4a7e077c2a67a3089455ba138ab93426607157
chikalnazmi <chikalnazmi05@gmail.com>
2024-10-02 17:31:00 +0700

    JS06-Praktikum 1 & 2

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,9 +4,9 @@
 <div class="card card-outline card-primary">
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
-
         <div class="card-tools">
             <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+            <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
         </div>
     </div>
 
@@ -46,6 +46,17 @@
         </table>
     </div>
 </div>
+
+<div 
+    id="myModal" 
+    class="modal fade animate shake" 
+    tabindex="-1" 
+    role="dialog" 
+    data-backdrop="static" 
+    data-keyboard="false" 
+    data-width="75%" 
+    aria-hidden="true">
+</div>
 @endsection
 
 @push('css')
@@ -53,8 +64,14 @@
 
 @push('js')
 <script>
-$(document).ready(function() {
-    var dataUser = $('#table_user').DataTable({
+    function modalAction(url = '') {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+    var dataUser;
+    $(document).ready(function() {
+        dataUser = $('#table_user').DataTable({
         // serverside: true, jika ingin menggunakan server side processing
         serverSide: true,
 
