6596f8bb0ca930fa1e689a072588864a0431ac39
chikalnazmi <chikalnazmi05@gmail.com>
2024-11-06 10:40:49 +0700

    JS10-Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,24 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; 
+use Yajra\DataTables\Html\Editor\Fields\Hidden;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-// class UserModel extends Model
-// {
-//     use HasFactory;
 
-//     protected $table = 'm_user';
-//     protected $primaryKey = 'user_id'; 
-//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-//     public function level(): BelongsTo
-//     {
-//         return $this->belongsTo(LevelModel::class,'level_id','level_id');
-//     }
-
-// }
-
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
 
     use HasFactory;
 
@@ -63,3 +59,18 @@ class UserModel extends Authenticatable
     }
 
 }
+
+// class UserModel extends Model
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user';
+//     protected $primaryKey = 'user_id'; 
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class,'level_id','level_id');
+//     }
+
+// }
